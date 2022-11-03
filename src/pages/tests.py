67ea14e002bb353995fef7b7c47ad285e9ec0bf4
taremwa <marktaremwa92@gmail.com>
2022-11-03 15:20:44 +0300

    initial tests commit

--- src/pages/tests.py
+++ src/pages/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import SimpleTestCase
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_status_code(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_explore_page_status_code(self):
+        response = self.client.get('/explore/')
+        self.assertEqual(response.status_code, 200)
--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from .views import HomeView, AboutView, ExploreView, ContactView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name="home.html"),
     path('about/', AboutView.as_view(), name="about.html"),
     path('explore/', ExploreView.as_view(), name="explore.html"),
